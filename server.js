93447dcb327ce5f86018f4e4c25a8c3efc456fbe
nohadeeb <39979559+nohadeeb@users.noreply.github.com>
2020-08-03 16:48:41 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const cors = require('cors');
+app.use(cors());
+// intializng the main project folder 
+//---------------------------  
+// create a local server with node nad express
+const port = 8000;
+//-- writing an arrow function isan effecient way 
+const server = app.listen(port, () => {
+    console.log('server running');
+    console.log(`running on localhost : ${port}`);
+});// to make sure that server isruning properly
+app.use(express.static('website'));
+const projectData = [];//array to store out data
+app.get('/getall', getApiData); // get the data tha was stored in the dataArray
+function getApiData(req,res){
+    res.send(dataArray);// send the response that was stored on our dataArray
+}
+app.post('/addAll', addWeatherDatafunc); // post request with a callback func addWeatherfunc
+function addWeatherDatafunc(req, res){
+    const newEntry={
+        temp: req.body.datatemp,
+        date: req.body.date,
+        content: content,
+        cloud: req.body.cloud,
+        feelslike: req.body.feelslike
+    }
+    console.log(newEntry);
+    projectData.push(newEntry); // push these data that we got from the api to our array
+}
---
+++ website/app.js
@@ -0,0 +1,72 @@
+
+
+document.getElementById('generate').addEventListener('click', performeAction);
+function performeAction(e) {
+    //Get the date
+    let d = new Date();
+    let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+    const baseurl = "http://api.openweathermap.org/data/2.5/weather?zip=";
+    const key = "&appid=eff0d8a1856b456cffe08f87e8f5884c";
+    const content = document.getElementById('fav').value;
+    const zip = document.getElementById('zip').value;
+    getData(baseurl, zip, key)
+        .then(function (data) {
+            console.log(data);
+            postData('http://localhost:8000/addAll', { date: newDate, datatemp: data.main.temp, content, cloud: data.clouds.all, feelslike: data.main.feels_like} )
+               
+        })
+         .then(
+             updateUI()
+         )
+    
+};
+const getData = async (baseurl, zip, key)=>{
+    const response = await fetch( baseurl + zip + key);
+    try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
+const postData = async (url='', data={}) => {
+    console.log(data);
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        // Body data type must match "Content-Type" header        
+        body: JSON.stringify(data),
+        
+    });
+    
+    try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+    } catch (error) {
+        console.log("error", error);
+    }
+ 
+}
+function changebackground(){
+    data.clouds.all.value > 3 ? document.getElementById('domUpdate').style.backgroundImage = "https://i.ibb.co/hmS7QtG/sunshine-sky-abstract-yellow-clouds-background.jpg" : document.getElementById('domUpdate').style.backgroundImage = "https://ibb.co/PGDH4jW";
+}
+const updateUI = async () => {
+    const request = await fetch('http://localhost:8000/getall');
+    try {
+        const allData = await request.json();
+        document.getElementById('date').innerHTML = newDate;
+        document.getElementById('temp').innerHTML = allData.temp;
+        document.getElementById('content').innerHTML = content;
+        document.getElementById('felslike').innerHTML = allData.feelslike;
+
+    } catch (error) {
+        console.log("error", error);
+    }
+    changebackground();
+}

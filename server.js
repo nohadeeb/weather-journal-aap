ae60674b557cf6feb332982afaf86fddf51f4dfe
nohadeeb <39979559+nohadeeb@users.noreply.github.com>
2020-08-05 15:51:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = app.listen(port, () => {
     console.log(`running on localhost : ${port}`);
 });// to make sure that server isruning properly
 app.use(express.static('website'));
-const projectData = {};//array to store out data
+let projectData = {};//object to store data
 app.get('/getall', getApiData); // get the data tha was stored in the dataArray
 function getApiData(req,res){
     res.send(projectData);// send the response that was stored on our dataArray
@@ -25,12 +25,13 @@ function getApiData(req,res){
 app.post('/addAll', addWeatherDatafunc); // post request with a callback func addWeatherfunc
 function addWeatherDatafunc(req, res){
     const newEntry={
-        temp: req.body.datatemp,
-        date: req.body.date,
-        content: content,
-        cloud: req.body.cloud,
-        feelslike: req.body.feelslike
+        temp: req.body.datatemp,//defined on postData line 15
+        date: req.body.date,//defined on postData line 15
+        feelings: req.body.feelings,//defined on postData line 15
+        cloud: req.body.cloud,//defined on postData line 15
+        feelslike: req.body.feelslike,//defined on postData line 15
+  
     }
     console.log(newEntry);
-    projectData.push(newEntry); // push these data that we got from the api to our array
+    projectData=newEntry; // if we stored data into an aarray we would use the push these data that we got from the api to our array
 }
